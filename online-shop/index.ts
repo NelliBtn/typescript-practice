74b995761b20445f6cae4d11a60e4e2cd8aee340
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-12 19:40:06 -0400

    free shipping depending on price

--- online-shop/index.ts
+++ online-shop/index.ts
@@ -1,21 +1,22 @@
 import products from './products';
 
 let productName: string;
-productName = 'tote bag';
-
 let shipping: number;
 let taxPercent: number;
 let taxTotal: number;
 let total: number;
 let shippingAdress: string;
+
+productName = 'hoodie';
 shippingAdress = '432 Park Ave New York'
 
-const product = products.filter(product => product.name === productName); // [ {} ]
+const product = products.filter(product => product.name === productName);
 
-const preOrder = product[0].preOrder === 'true' ? true : false; // convert string to boolean
+const preOrder = product[0].preOrder === 'true' ? true : false;
 
 if (preOrder) {
   console.log('We will send you a message when the product is on its way');
-}
+};
 
-console.log(product[0])
+Number(product[0].price) >= 25 ? shipping = 0 : shipping = 5;
+console.log(shipping);
