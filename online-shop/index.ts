16340256fe7feefbac10a124f2488a00f385aac6
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-12 19:45:39 -0400

    set up tax percent for new york and add comments

--- online-shop/index.ts
+++ online-shop/index.ts
@@ -10,13 +10,19 @@ let shippingAdress: string;
 productName = 'hoodie';
 shippingAdress = '432 Park Ave New York'
 
+// find product by name
 const product = products.filter(product => product.name === productName);
 
-const preOrder = product[0].preOrder === 'true' ? true : false;
+// convert preOrder string to boolean
+const preOrderBool = product[0].preOrder === 'true' ? true : false;
 
-if (preOrder) {
+// if preOrder is available send a msg to customer
+if (preOrderBool) {
   console.log('We will send you a message when the product is on its way');
 };
 
+// if product price over 25 shipping is free
 Number(product[0].price) >= 25 ? shipping = 0 : shipping = 5;
-console.log(shipping);
+
+// if shipping to NY tax percent is 10%, otherwise 5%
+shippingAdress.includes('New York') ? taxPercent = 0.1 : taxPercent = 0.05;
