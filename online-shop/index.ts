eb0b95293d3c75dab037dc3cb71ad0591ebb0b24
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-12 19:10:02 -0400

    find product by its name

--- online-shop/index.ts
+++ online-shop/index.ts
@@ -2,3 +2,7 @@ import products from './products';
 
 let productName: string;
 productName = 'tote bag';
+
+const product = products.filter(product => product.name === productName);
+
+console.log(product)
