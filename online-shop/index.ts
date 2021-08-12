4dbc0a9fa10f28ba88e96e6e4c037a75fd783a5e
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-12 19:35:36 -0400

    declare receipt and shipping variables and types

--- online-shop/index.ts
+++ online-shop/index.ts
@@ -3,6 +3,13 @@ import products from './products';
 let productName: string;
 productName = 'tote bag';
 
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+let shippingAdress: string;
+shippingAdress = '432 Park Ave New York'
+
 const product = products.filter(product => product.name === productName); // [ {} ]
 
 const preOrder = product[0].preOrder === 'true' ? true : false; // convert string to boolean
