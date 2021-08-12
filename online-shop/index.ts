f9e327bdf503d336af41837650f60eeabca7d27d
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-12 19:31:53 -0400

    check if preorder is available, convert string to boolean

--- online-shop/index.ts
+++ online-shop/index.ts
@@ -3,6 +3,12 @@ import products from './products';
 let productName: string;
 productName = 'tote bag';
 
-const product = products.filter(product => product.name === productName);
+const product = products.filter(product => product.name === productName); // [ {} ]
 
-console.log(product)
+const preOrder = product[0].preOrder === 'true' ? true : false; // convert string to boolean
+
+if (preOrder) {
+  console.log('We will send you a message when the product is on its way');
+}
+
+console.log(product[0])
