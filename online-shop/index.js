d3f5b9094bbff04eddd164439b46c6ea7178f1ed
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-12 20:04:05 -0400

    calculate total and add price variable

--- online-shop/index.js
+++ online-shop/index.js
@@ -2,6 +2,27 @@
 exports.__esModule = true;
 var products_1 = require("./products");
 var productName;
-productName = 'tote bag';
+var shipping;
+var taxPercent;
+var taxTotal;
+var total;
+var shippingAdress;
+productName = 'hoodie';
+shippingAdress = '432 Park Ave New York';
+// find product by name
 var product = products_1["default"].filter(function (product) { return product.name === productName; });
-console.log(product);
+// convert preOrder string to boolean
+var preOrderBool = product[0].preOrder === 'true' ? true : false;
+// if preOrder is available send a msg to customer
+if (preOrderBool) {
+    console.log('We will send you a message when the product is on its way');
+}
+;
+var productPrice = Number(product[0].price);
+// if product price over 25 shipping is free
+Number(productPrice) >= 25 ? shipping = 0 : shipping = 5;
+// if shipping to NY tax percent is 10%, otherwise 5%
+shippingAdress.match('New York') ? taxPercent = 0.1 : taxPercent = 0.05;
+// calculate total
+taxTotal = productPrice * taxPercent;
+total = taxTotal + productPrice + shipping;
--- online-shop/index.ts
+++ online-shop/index.ts
@@ -21,8 +21,16 @@ if (preOrderBool) {
   console.log('We will send you a message when the product is on its way');
 };
 
+const productPrice = Number(product[0].price);
+
 // if product price over 25 shipping is free
-Number(product[0].price) >= 25 ? shipping = 0 : shipping = 5;
+Number(productPrice) >= 25 ? shipping = 0 : shipping = 5;
 
 // if shipping to NY tax percent is 10%, otherwise 5%
-shippingAdress.includes('New York') ? taxPercent = 0.1 : taxPercent = 0.05;
+shippingAdress.match('New York') ? taxPercent = 0.1 : taxPercent = 0.05;
+
+// calculate total
+taxTotal = productPrice * taxPercent;
+total = taxTotal + productPrice + shipping;
+
+
