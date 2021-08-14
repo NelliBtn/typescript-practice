fd8e9e2b17533726949f087ab8119c733880b44b
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-14 19:44:40 -0400

    default finction parameters;

---
+++ proclaim.ts
@@ -0,0 +1,11 @@
+// DEFAULT PARAMETERS
+
+function proclaim(status = "not ready...", repeat = 1) {
+  for (let i = 0; i < repeat; i += 1) {
+    console.log(`I'm ${status}`);
+  }
+}
+
+proclaim();
+proclaim('ready?');
+proclaim('ready!', 3);
