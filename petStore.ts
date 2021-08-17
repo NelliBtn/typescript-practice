e8defa8adcae70a431b8f59303e71c5215b34676
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-16 20:10:33 -0400

    enums in TS

---
+++ petStore.ts
@@ -0,0 +1,27 @@
+let petOnSale = 'chinchilla';
+let ordersArray = [
+  ['rat', 2],
+  ['chinchilla', 1],
+  ['hamster', 2],
+  ['chinchilla', 50]
+];
+
+// Instead use TS enums:
+
+enum Pet {
+  Hamster,
+  Rat,
+  Chinchilla,
+  Tarantula
+}
+
+let petOnSaleTS: Pet = Pet.Chinchilla;
+
+let ordersArrayTS: [Pet, number][] = [
+  [Pet.Rat, 2],
+  [Pet.Chinchilla, 1],
+  [Pet.Hamster, 2],
+  [Pet.Chinchilla, 50]
+];
+
+ordersArrayTS.push([Pet.Jerboa, 3]) // error 'Property 'Jerboa' does not exist on type 'typeof Pet''
